86d123f43dbd3cf840e22aa39fc832fe6b59f6c1
ahah43 <ahah432@yahoo.com>
2022-09-05 22:30:36 +0400

    Add files via upload

---
+++ pkg/without_a_bundler_bare_for.d.ts
@@ -0,0 +1,47 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} a
+* @param {number} b
+* @returns {number}
+*/
+export function add(a: number, b: number): number;
+/**
+* @param {string} dia_string
+* @param {string} th_string
+* @param {string} availables
+* @param {string} user_tol_string
+* @returns {string}
+*/
+export function my_core_design(dia_string: string, th_string: string, availables: string, user_tol_string: string): string;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly add: (a: number, b: number) => number;
+  readonly my_core_design: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+}
+
+/**
+* Synchronously compiles the given `bytes` and instantiates the WebAssembly module.
+*
+* @param {BufferSource} bytes
+*
+* @returns {InitOutput}
+*/
+export function initSync(bytes: BufferSource): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
